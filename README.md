8dc800cfc1569e633cb0926df097e568d89ea3a6
Jean Maciel <jeanvictorg@hotmail.com>
2024-06-13 13:35:42 -0300

    Configurando automação de tarefas no windows

--- README.md
+++ README.md
@@ -8,10 +8,13 @@ Como buildar o Projeto:
 Requisitos: Java 17 ou superior, Maven e PostgreSQL
 
 Windows:
-1) Execute como Administrador o arquivo "configure.bat" no windows, no final, ele vai pedir os dados de conexão com o banco de dados
-2) Execute o jar na raiz do projeto
+1) Execute como Administrador o arquivo "configure.bat" no windows, em seguida, preencha os dados de conexão com o banco.
+2) O projeto irá iniciar automaticamente após preencher os dados.
+2) Caso tenha preenchido os dados incorretamente, execute o "configure.bat" siga os passos novamente.
+4) O Projeto ficará na pasta build, basta executar o jar com "java -jar app-1.0.jar"
 
 Linux:
-1) Dê permissão ao arquivo "configure.sh" com "sudo chmod +rwx configure.sh"
-2) Execute com "./configure.sh" e informe os dados de conexão com o banco
+1) Dê permissão ao arquivo "configure.sh" com "sudo chmod +rwx configure.sh".
+2) Execute com "./configure.sh" e informe os dados de conexão com o banco.
 3) Após ter preenchido corretamente, o projeto será iniciado automaticamente, caso erre os dados de conexão, basta executar o "configure.sh" novamente e repetir o processo.
+4) O Projeto ficará na pasta build, basta executar o jar com "java -jar app-1.0.jar"
--- configure.bat
+++ configure.bat
@@ -1,29 +1,42 @@
 @echo off
+echo ---------------------------
 echo Limpando o Projeto...
 cd %~dp0
-RD /S /Q libs
-del /Q /F app-*
-DEL /Q /F config\database\connection.properties
-cd %~dp0\app
-call mvn clean
-timeout /t 3
-
-echo Compilando o projeto...
-call mvn package
-timeout /t 3
-
-echo Reorganizando os arquivos do projeto...
-move /y target\app-1.0.jar ..\
-move /y target\libs ..\
-cd %~dp0
+IF EXIST build RD /S /Q build
+IF EXIST config\database\connection.properties DEL /Q /F config\database\connection.properties
+echo ---------------------------
+echo Configurando o banco
 
 set /p url="Informe a url jdbc: "
-set /p usr="Informe o login de usuário: "
-set /p pwd="Informe a senha do usuário: "
+set /p usr="Informe o usuario: "
+set /p pwd="Informe a senha: "
 
 echo url=%url%>>config\database\connection.properties
 echo usr=%usr%>>config\database\connection.properties
 echo pwd=%pwd%>>config\database\connection.properties
-
+echo ---------------------------
+echo Compilando o projeto...
+cd %~dp0\app
+call mvn package
+if errorlevel 1 (
+    echo Erro ao compilar o projeto
+    pause
+    exit /b 1
+)
+timeout /t 3
+echo ---------------------------
+echo Reorganizando os arquivos do projeto...
+mkdir ..\build
+move target\app-1.0.jar ..\build
+move target\libs ..\build
+cd %~dp0
+xcopy /E /I config build\config
+xcopy /E /I resources build\resources
+echo ---------------------------
 echo Configurado com sucesso
+echo ---------------------------
+echo Executando o Projeto
+echo ---------------------------
+cd %~dp0\build
+call java -jar app-1.0.jar
 pause
